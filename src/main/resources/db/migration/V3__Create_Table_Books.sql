2edde01fc5036859301ab3068d7e7b67ecfcdde8
Thayana <thay.c.gayv@outlook.com>
2024-08-26 21:26:11 -0400

    Implementation migration Books

---
+++ src/main/resources/db/migration/V3__Create_Table_Books.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE `books` (
+  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+  `author` longtext NOT NULL,
+  `publication_year` datetime(6),
+  `price` decimal(65,2) NOT NULL,
+  `title` longtext  NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
