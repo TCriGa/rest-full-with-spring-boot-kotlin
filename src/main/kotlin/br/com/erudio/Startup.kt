cdd4157b32e0779ed887cdedeedd9deca89222d6
Thayana <thay.c.gayv@outlook.com>
2024-09-03 16:29:36 -0400

    implementation test container

--- src/main/kotlin/br/com/erudio/Startup.kt
+++ src/main/kotlin/br/com/erudio/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class startup
+class Startup
 
 fun main(args: Array<String>) {
-	runApplication<startup>(*args)
+	runApplication<Startup>(*args)
 }
---
+++ src/test/kotlin/br/com/erudio/ConfigsTest.kt
@@ -0,0 +1,5 @@
+package br.com.erudio
+
+object ConfigsTest {
+    const val SERVER_PORT = 8888
+}
