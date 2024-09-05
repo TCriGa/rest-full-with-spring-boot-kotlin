e9e05747d96fdae4604ecce5a994ac743d9cd203
Thayana <thay.c.gayv@outlook.com>
2024-09-05 15:22:41 -0400

    implementation and config test container

--- src/test/kotlin/br/com/erudio/integrationtests/testcontainer/AbstractIntegrationTest.kt
+++ src/test/kotlin/br/com/erudio/integrationtests/testcontainer/AbstractIntegrationTest.kt
@@ -1,43 +1,56 @@
 package br.com.erudio.integrationtests.testcontainer
 
-import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.core.env.MapPropertySource
-import org.springframework.test.context.ContextConfiguration
+import br.com.erudio.ConfigsTest
+import br.com.erudio.repository.PersonRepository
+import io.restassured.RestAssured
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.lifecycle.Startables
-import java.util.stream.Stream
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@ContextConfiguration(initializers = [AbstractIntegrationTest.Initializer::class])
-open class AbstractIntegrationTest {
 
-    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext>{
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Testcontainers
+abstract class AbstractIntegrationTest {
+    companion object {
+        var mysql = MySQLContainer("mysql:9.0.1")
 
-        override fun initialize(applicationContext: ConfigurableApplicationContext) {
-            startContainers()
+        @LocalServerPort
+        val serverPort = ConfigsTest.SERVER_PORT
 
-            val environment = applicationContext.environment
-            val testcontainers = MapPropertySource(
-                "testcontainers", createConnectionConfiguration()
-            )
-            environment.propertySources.addFirst(testcontainers)
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            mysql.start()
         }
 
-        companion object {
-
-            private var mysql: MySQLContainer<*> = MySQLContainer("mysql:8.0.28")
-
-            private fun startContainers() {
-                Startables.deepStart(Stream.of(mysql)).join()
-            }
-
-            private fun createConnectionConfiguration(): MutableMap<String, Any> {
-                return java.util.Map.of(
-                    "spring.datasource.url", mysql.jdbcUrl,
-                    "spring.datasource.username", mysql.username,
-                    "spring.datasource.password", mysql.password,
-                )
-            }
+        @JvmStatic
+        @AfterAll
+        fun afterAll() {
+            mysql.stop()
         }
+
+        @Autowired
+        var personRepository: PersonRepository? = null
+
+        @BeforeEach
+        fun setUp() {
+            RestAssured.baseURI = "http://localhost:$serverPort"
+            personRepository?.deleteAll()
+        }
+
+        @DynamicPropertySource
+        fun configureProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysql::getJdbcUrl)
+            registry.add("spring.datasource.username", mysql::getUsername)
+            registry.add("spring.datasource.password", mysql::getPassword)
+        }
+
     }
 }
