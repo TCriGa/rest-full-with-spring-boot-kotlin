24b7f74252ad77eabf744822318c2881ba37fe7a
Thayana <thay.c.gayv@outlook.com>
2024-10-10 15:16:12 -0400

    Implementation configuration and logic service and response

---
+++ src/main/kotlin/br/com/erudio/date/vo/v1/UploadFileResponseVO.kt
@@ -0,0 +1,8 @@
+package br.com.erudio.date.vo.v1
+
+class UploadFileResponseVO (
+    var fileName: String = "",
+    var fileDownloadUri: String = "",
+    var fileType: String = "",
+    var fileSize: Long = 0
+)
